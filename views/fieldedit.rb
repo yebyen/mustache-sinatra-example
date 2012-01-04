f0b7ab4aa3909d2d5074993940e5b6455701ccf5
Kingdon Barrett <kbarrett@metrixmatrix.com>
2012-01-04 09:59:01 -0500

    enhancements to fieldedit

--- views/fieldedit.rb
+++ views/fieldedit.rb
@@ -12,30 +12,39 @@ class App
         @@have
       end
       def self.update(db, record)
-	schema=record["schema"]
-	table=record["table"]
-	column=record["column"]
-	notes=record["notes"]
-	status=record["status"]
+	schema=db.escape(record["schema"])
+	table=db.escape(record["table"])
+	column=db.escape(record["column"])
+	notes=db.escape(record["notes"])
+	status=db.escape(record["status"])
 
 	results=db.query("UPDATE field_definitions SET notes='#{notes}', status='#{status}' WHERE table_schema='#{schema}' AND table_name='#{table}' AND column_name='#{column}'")
 	# Check for error status maybe?
       end
       def self.row(db, params)
+
+      db1=Mysql2::Client.new(:host => 'db0', :username => 'kbarrett',
+	:password => File.new("pass","r").gets, :database => params[:splat][0])
+
 	data=nil
 	if ! params[:splat].nil?
-	  schema=params[:splat][0]
-	  table=params[:splat][1]
-	  column=params[:splat][2]
+	  schema=db.escape(params[:splat][0])
+	  table=db.escape(params[:splat][1])
+	  column=db.escape(params[:splat][2])
 
+	  results=db.query("SELECT notes, status FROM field_definitions WHERE table_schema='#{schema}' AND table_name='#{table}' AND column_name='#{column}'")
 	  data={
 	    "schema" => schema,
 	    "table" => table,
 	    "column" => column,
-	    "row"=>{} }
-	  results=db.query("SELECT notes, status FROM field_definitions WHERE table_schema='#{schema}' AND table_name='#{table}' AND column_name='#{column}'")
+	    "row"=>{},
+	    "data"=>[] }
 	  results.each do |row|
 	    data["row"]=row
+	  end
+	  result=db1.query("SELECT DISTINCT `#{column}` FROM `#{table}` LIMIT 20")
+	  result.each do |row|
+	    data["data"]<<{"value"=> "<pre style='margin: 0px;'>" << row[column].to_s << "</pre>"}
 	  end
 	end
 	data
